5ff2617c91b252ba6171eeaed0928b62b4b7b4bb
mwagner <mwagner@students.42wolfsburg.de>
2023-02-21 17:44:29 +0100

    more funcs level 0

---
+++ 42EXAMS/.subjects/STUD_PART/exam_02/0/max/ulstr.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	char *str;
+
+	i = 0;
+
+	if (argc == 2)
+		str = argv[1];
+		while (str[i] != '\0')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+			write(1, &str[i], 1);
+				i++;
+			
+		}
+	write (1, "\n", 1);
+	return(0);
+}
