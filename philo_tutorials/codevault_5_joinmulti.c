837dc9c236a318a1979817dccb90f6c9b0698b81
mwagner <mwagner@student.42wolfsburg.de>
2023-02-21 14:58:24 +0100

    joinmultithread self added

--- philo_tutorials/codevault_5_joinmulti.c
+++ philo_tutorials/codevault_5_joinmulti.c
@@ -3,13 +3,17 @@
 #include <pthread.h>
 #include <time.h>
 
+pthread_mutex_t mutex;
+
 void *roll_dice()
 {
+	pthread_mutex_lock(&mutex);
 	int value = (rand() % 6) + 1;
+	pthread_mutex_unlock(&mutex);
 	int *result = malloc(sizeof(int));
 	*result = value;
 	// printf("%d\n", value);
-	printf("Thread result memory address result: %p\n", result);
+	// printf("Thread result memory address result: %p\n", result);
 	return (void*) result;
 }
 
@@ -17,17 +21,27 @@ int main(int argc, char *argv[])
 {
 	int *res;
 	srand(time(NULL));
-	pthread_t th;
-	if (pthread_create(&th, NULL, &roll_dice, NULL) !=0)
+	pthread_t th[8];
+	int i;
+	pthread_mutex_init(&mutex, NULL);
+	for (i = 0; i < 8; i++)
 	{
-		return 1;
+		if (pthread_create(th + i, NULL, &roll_dice, NULL) !=0)
+		{
+			perror("Failed to create thread");
+			return 1;
+		}
+		printf("Thread %d has started\n", i);
 	}
-	if (pthread_join(th, (void**) &res) != 0)
+
+	for (i = 0; i < 8; i++)
 	{
-		return 2;
+		if (pthread_join(th[i], (void**) &res) != 0)
+			return 2;
+		printf("Thread %d has finished execution\n", i);
+		printf("Result: %d\n", *res);
+		free(res);
 	}
-	printf("Main result memory address result: %p\n", res);
-	printf("Result: %d\n", *res);
-	free(res);
+	pthread_mutex_destroy(&mutex);
 	return 0;
 }
