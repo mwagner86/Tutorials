8a6b57f40af7552694a676952b0bd25b4e789b59
mwagner <mwagner@student.42wolfsburg.de>
2023-03-01 12:26:29 +0100

    fizzbuzz with putnbr

--- 42EXAMS/.subjects/STUD_PART/exam_02/0/max/fizzbuzz.c
+++ 42EXAMS/.subjects/STUD_PART/exam_02/0/max/fizzbuzz.c
@@ -1,10 +1,16 @@
 #include <unistd.h>
 
+void	ft_putnbr(int nbr)
+{
+	if (nbr >=10)
+		ft_putnbr(nbr / 10);
+	char c = (nbr % 10) + 48;
+	write (1, &c, 1);
+}
+
 int main(void)
 {
 	int i;
-	int k;
-	int l;
 
 	i = 1;
 	while (i <= 100)
@@ -15,18 +21,8 @@ int main(void)
 				write(1, "fizz", 4);
 			else if (i % 5 == 0)
 				write(1, "buzz", 4);
-			else if (i > 10)
-			{
-				k = i / 10 + '0';
-				l = i % 10 + '0';
-				write(1, &k, 1);
-				write(1, &l, 1);
-			}
 			else
-			{
-				k = i + '0';
-				write(1, &k, 1);
-			}
+				ft_putnbr(i);
 			write(1, "\n", 1);
 			i++;
 		}
